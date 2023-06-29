eec6ba76e795dbf5f84a0dda4dddaaf988b249a5
Vadim Kravcov <v.sem4ik@mail.ru>
2023-06-29 15:31:30 +0300

    add end prod without tests

--- kursovaya_3/vidget_code.py
+++ kursovaya_3/vidget_code.py
@@ -1,50 +1,53 @@
 
-import json
 from datetime import datetime
-
-with open("operations.json","rt") as file:
-    content = file.read()
-
-# print(content)
-    # for el in file:
-    #     print(el)
-    #     print()
-
-content = json.loads(content)
-# print(content[1])
-# print()
-# print(content[1]['operationAmount']['currency'])
-
-executed_operations_list = []
-
-for i in content:
-    item = i.get('state', 'no info')
-    # print(item)
-    # print()
-    if item =='EXECUTED':
-        executed_operations_list.append(i)
+import functions
 
 
-#print(executed_operations_list)
+content = functions.get_file_data("operations.json")
 
-thedate = datetime.fromisoformat(content[1]['date'])
-thedate2 = datetime.fromisoformat(content[2]['date'])
+executed_operations_list = functions.do_sort_by_approv(content)
 
-# print(thedate<thedate2)
-# print(thedate)
+functions.buble_sort(executed_operations_list)
 
-dlin = len(executed_operations_list)
+#Код ниже запускает иттерацию по пяти последним операциям
 
-for i in range(dlin - 1):
-    for j in range(dlin - i - 1):
-        if datetime.fromisoformat(executed_operations_list[j]['date']) > datetime.fromisoformat(executed_operations_list[j + 1]['date']):
-            executed_operations_list[j], executed_operations_list[j + 1] = executed_operations_list[j + 1], executed_operations_list[j]
-
-# print(executed_operations_list[:-6:-1])
-# print()
 for i in executed_operations_list[:-6:-1]:
-    print(f"{i['date']} {i['description']}")
-    print(f"{i.get('from', 'no info')} - {i['to']}")
+
+    # Эта срочка будет использоваться ниже для вывода даты по порядку
+    dates = datetime.fromisoformat(i['date'])
+
+    # Эта часть кода подстраивает номер карты под требования
+    card_number = i.get('from', 'no info')
+    number = []
+    if len(card_number)>7:
+        card_number=card_number.split()
+        number = [i for i in card_number[-1]]
+        for k in [-5,-6,-7,-8,-9,-10]:
+            number[k] = "*"
+        number.insert(-4, " ")
+        number.insert(-9, " ")
+        number.insert(-14, " ")
+        number.insert(-19, " ")
+        number = "".join(number)
+        #print(number)
+        if len(list(card_number)[-1])>7:
+            card_number = list(card_number)
+            card_number.pop(-1)
+            card_number.append(number)
+            card_number = " ".join(card_number)
+
+    #Эта часть кода подстраивает номер СЧЁТА под требования
+    account_number = i['to']
+    acc_number = ['*','*']
+    account_number = [i for i in account_number]
+    for z in [-4,-3,-2,-1]:
+        acc_number.append(account_number[z])
+    acc_number = "".join(acc_number)
+
+
+    # Здесь вывод всей информации
+    print(f"{dates.day}.{dates.month}.{dates.year} {i['description']}")
+    print(f"{card_number} -> {acc_number}")
     print(f"{i['operationAmount']['amount']} {i['operationAmount']['currency']['name']}")
     print()
 
